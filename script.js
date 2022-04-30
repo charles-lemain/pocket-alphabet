9c01930f71d15ef30cb094f0b47129041d45448a
Karol <kstachowicz@icloud.com>
2022-04-30 04:43:14 +0200

    First working version

--- script.js
+++ script.js
@@ -1,10 +1,60 @@
-const letterBox = document.querySelector('#letter-show');
+function createAlphabet () {
+    let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','W','X','Y','Z'];
+    console.log(alphabet);
+    return alphabet;
+}
 
-letterBox.addEventListener('click', (event) => {
+function getRandomInt (currentRange) {
+    const min = Math.ceil(0);
+    const max = Math.floor(currentRange-1);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function removeFromArray (array, indexToRemove) {
+    
+    let usedLetter = array.splice(indexToRemove, 1);
+    usedArray.push(usedLetter[0]);
+    return usedLetter;
+
+}
+
+function arrayToTextContent (arrayUsed) {
+    let stringArray = '';
+    for (let i = 0; i < arrayUsed.length; i++) {
+        stringArray = stringArray + arrayUsed[i] + ' ';
+    }
+    return stringArray;
+}
+
+const letterBox = document.querySelector('#letter-show');
+const startButton = document.querySelector('#start-button');
+const resetButton = document.querySelector('#reset-button');
+const usedLetters = document.querySelector('#used-letters');
+
+let letterArray = createAlphabet();
+let usedArray = [];
+startButton.addEventListener('click', (event, currentIndex) => {
     console.log(event.target.id)
-    let textContent = event.target.textContent;
-    console.log(textContent);
-    textContent = 'I';
-    console.log(textContent);
+    let currentLetter = removeFromArray(letterArray, getRandomInt(letterArray.length));
+
+    letterBox.textContent = currentLetter;
+    if (usedArray.length < 26) {
+        usedLetters.textContent = arrayToTextContent(usedArray);
+    }
+
+    
+    
+
+    console.log(currentLetter);
+    //console.log(letterArray);
+    
 
 });
+
+resetButton.addEventListener('click', (event) => {
+    letterArray = createAlphabet();
+    usedArray = [];
+    usedLetters.textContent = '';
+    letterBox.textContent = '';
+});
+
